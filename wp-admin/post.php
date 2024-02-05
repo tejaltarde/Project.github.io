fc45516ee653538c2412b8bb7cd7add6b9bb7f59
tejaltarde <141155278+tejaltarde@users.noreply.github.com>
2024-02-05 22:28:57 +0530

    Update post.php

--- wp-admin/post.php
+++ wp-admin/post.php
@@ -6,7 +6,7 @@
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "https://tejaltarde.github.io/Project.github.io//wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FSearchinTech%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D24895&reauth=1";
+			window.location = "http://localhost/SearchinTech/wp-admin/post.php?post=413&action=elementor";
 		</script>
 
 		<p>You are being redirected to <a href="https://tejaltarde.github.io/Project.github.io//wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FSearchinTech%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D24895&reauth=1">https://tejaltarde.github.io/Project.github.io//wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FSearchinTech%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D24895&reauth=1</a></p>
